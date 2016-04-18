bf7b4e39c40b88b42c5ef34b7448b4f21d9ebb9e
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-04-18 13:53:10 +0200

    group of probes pk lookup fixed

--- poem/Poem/poem/admin_interface/formmodel.py
+++ poem/Poem/poem/admin_interface/formmodel.py
@@ -139,7 +139,7 @@ class MyFilteredSelectMultiple(FilteredSelectMultiple):
         elif self.selformname == 'probes':
             for sel in selected_choices:
                 output.append('<option value="%s" selected="selected">' % (sel)
-                              + str(Probe.objects.get(id=int(sel)).nameversion)
+                              + str(Probe.objects.get(nameversion=sel))
                               + '</option>\n')
         for option_value, option_label in chain(self.choices, choices):
             if isinstance(option_label, (list, tuple)):
