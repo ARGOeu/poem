b2100fd7c0b460c0e300fe0d7f99adbf6f4227c4
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-05-30 18:21:36 +0200

    SSL logout redirect

--- poem/Poem/poem/templatetags/ssl.py
+++ poem/Poem/poem/templatetags/ssl.py
@@ -6,12 +6,12 @@ register = template.Library()
 @register.filter(name='to_https')
 def to_https(value):
     return value.replace('http', 'https')
-    
+
 @register.simple_tag
 def to_http(abs_url, path):
     url_obj = list(urlparse.urlparse(abs_url))
     if len(url_obj) > 2:
         url_obj[0] = 'http'
         url_obj[2] = path
-    
-    return (urlparse.urlunparse(url_obj))    
+
+    return (urlparse.urlunparse(url_obj))
