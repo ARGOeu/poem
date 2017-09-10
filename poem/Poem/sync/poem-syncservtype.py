ec14d04b0b55f7e6e44969876c62ecc7395beccf
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-09-10 21:01:05 +0200

    fix servtype sync count

--- poem/Poem/sync/poem-syncservtype.py
+++ poem/Poem/sync/poem-syncservtype.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
 
 import string
-import sys
-import urllib2
 import httplib
 import os
 import logging
-import Poem.django_logging
 from Poem import settings
 from Poem.poem import models
 from django.db import connection, transaction
@@ -70,21 +67,21 @@ def main():
 
 
     sfindb = set([(sf.name, sf.description) for sf in models.ServiceFlavour.objects.all()])
-    if len(sfindb) != len(Feed_List) + 1:
+    if len(sfindb) != len(Feed_List):
         sfs = set([(feed['service_type_name'], feed['service_type_desc']) \
                 for feed in Feed_List])
         cur = connection.cursor()
         try:
-            if len(sfindb) < len(Feed_List) + 1:
+            if len(sfindb) < len(Feed_List):
                 cur.executemany('INSERT INTO poem_serviceflavour VALUES (?,?)', \
                         sfs.difference(sfindb))
                 logger.info("Added %d service flavours" %\
-                            (len(Feed_List) + 1 - len(sfindb)))
-            elif len(sfindb) > len(Feed_List) + 1:
+                            (len(Feed_List) - len(sfindb)))
+            elif len(sfindb) > len(Feed_List):
                 cur.executemany('DELETE FROM poem_serviceflavour WHERE name IN (?,?)', \
                         sfindb.difference(sfs))
                 logger.info("Deleted %d service flavours" %\
-                            (len(sfindb) - len(Feed_List) + 1))
+                            (len(sfindb) - len(Feed_List)))
             transaction.commit_unless_managed()
             connection.close()
         except Exception as e:
