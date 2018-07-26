af2ea4106f9f62c054724fc7b300d7d8d0ff2706
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-07-27 00:15:46 +0200

    adapt perms on poem virtenv directory only

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,10 +5,10 @@ source /etc/profile.d/venv_poem.sh
 workon poem
 
 # prerequisites
-poem-genseckey -f $WORKON_HOME/poem/etc/poem/secret_key
-cp -f poem.conf $WORKON_HOME/poem/etc/poem/poem.conf
-chown -R apache:apache $WORKON_HOME
+poem-genseckey -f $VIRTUAL_ENV/etc/poem/secret_key
+cp -f poem.conf $VIRTUAL_ENV/etc/poem/poem.conf
+chown -R apache:apache $VIRTUAL_ENV
 poem-db -c
-ln -f -s $WORKON_HOME/poem/etc/httpd/conf.d/poem.conf /etc/httpd/conf.d/
-ln -f -s $WORKON_HOME/poem/etc/cron.d/poem-clearsessions /etc/cron.d/
-ln -f -s $WORKON_HOME/poem/etc/cron.d/poem-syncvosf /etc/cron.d/
+ln -f -s $VIRTUAL_ENV/etc/httpd/conf.d/poem.conf /etc/httpd/conf.d/
+ln -f -s $VIRTUAL_ENV/etc/cron.d/poem-clearsessions /etc/cron.d/
+ln -f -s $VIRTUAL_ENV/etc/cron.d/poem-syncvosf /etc/cron.d/
