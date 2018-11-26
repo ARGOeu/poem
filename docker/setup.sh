a99b9a6ccaede99c00c6ef95592a905804cf6bd0
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-11-26 19:52:35 +0100

    updated Dockerfile with Apache SCL installation

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,6 +9,6 @@ poem-genseckey -f $VIRTUAL_ENV/etc/poem/secret_key
 cp -f poem.conf $VIRTUAL_ENV/etc/poem/poem.conf
 chown -R apache:apache $VIRTUAL_ENV
 poem-db -c
-ln -f -s $VIRTUAL_ENV/etc/httpd/conf.d/poem.conf /etc/httpd/conf.d/
+ln -f -s $VIRTUAL_ENV/etc/httpd/conf.d/poem.conf /opt/rh/httpd24/root/etc/httpd/conf.d/
 ln -f -s $VIRTUAL_ENV/etc/cron.d/poem-clearsessions /etc/cron.d/
 ln -f -s $VIRTUAL_ENV/etc/cron.d/poem-syncvosf /etc/cron.d/
