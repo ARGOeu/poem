39d12a7a030ca808eab2def7ea4ab84a7fb2fa24
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-12-11 21:36:31 +0100

    new profiles API

--- poem/Poem/api/views.py
+++ poem/Poem/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+
+from Poem.poem import models
+
+class ListProfile(generics.ListAPIView):
+    queryset = models.Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+class DetailProfile(generics.RetrieveAPIView):
+    lookup_field = 'name'
+    queryset = models.Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
--- poem/Poem/urls_api.py
+++ poem/Poem/urls_api.py
@@ -5,8 +5,8 @@ from Poem.poem import views
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^0.2/json/profiles/?$', views.Profiles.as_view()),
-    url(r'^0.2/json/metrics_in_profiles/?$', views.MetricsInProfiles.as_view()),
-    url(r'^0.2/json/metrics_in_group/?$', views.MetricsInGroup.as_view()),
-    url(r'^0.2/json/metrics/?$', views.Metrics.as_view())
+    url(r'^json/profiles/?$', views.Profiles.as_view()),
+    url(r'^json/metrics_in_profiles/?$', views.MetricsInProfiles.as_view()),
+    url(r'^json/metrics_in_group/?$', views.MetricsInGroup.as_view()),
+    url(r'^json/metrics/?$', views.Metrics.as_view())
 ]
