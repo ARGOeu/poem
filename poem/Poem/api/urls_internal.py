25769814b861d58534f279bd832a294c0b0581b9
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-12-18 22:27:03 +0100

    get tokens API

--- poem/Poem/api/urls_internal.py
+++ poem/Poem/api/urls_internal.py
@@ -4,5 +4,7 @@ from . import views_internal
 
 urlpatterns = [
     path('metrics/<str:group>', views_internal.ListMetricsInGroup.as_view()),
+    path('tokens/', views_internal.ListTokens.as_view()),
+    path('tokens/<str:name>', views_internal.ListTokenForTenant.as_view()),
 ]
 
--- poem/Poem/api/views_internal.py
+++ poem/Poem/api/views_internal.py
@@ -2,15 +2,42 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 
-from Poem.poem import models
+from rest_framework_api_key import models as api_models
+
+from Poem.poem import models as poem_models
+
+from .views import NotFound
 
 
 class ListMetricsInGroup(APIView):
     authentication_classes = (SessionAuthentication,)
 
     def get(self, request, group):
-        metrics = models.Metrics.objects.\
+        metrics = poem_models.Metrics.objects.\
             filter(groupofmetrics__name__exact=group).\
             values_list('name', flat=True)
         results = sorted(metrics, key=lambda m: m.lower())
         return Response({'result': results})
+
+
+class ListTokens(APIView):
+    authentication_classes = (SessionAuthentication,)
+
+    def get(self, request):
+        tokens = api_models.APIKey.objects.all().values_list('client_id', 'token')
+        api_format = [dict(name=e[0], token=e[1]) for e in tokens]
+        return Response(api_format)
+
+
+class ListTokenForTenant(APIView):
+    authentication_classes = (SessionAuthentication,)
+
+    def get(self, request, name):
+        try:
+            e = api_models.APIKey.objects.get(client_id=name)
+
+            return Response(e.token)
+
+        except api_models.APIKey.DoesNotExist:
+            raise NotFound(status=404,
+                           detail='Tenant not found')
