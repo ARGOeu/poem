6e1752f32b9bc2605bad5edf9910bcb00f9fec89
Katarina Zailac <katarina.zailac@gmail.com>
2018-12-27 12:00:10 +0100

    Added unit test for internal_views to check if metrics are obtained properly by get method.

--- poem/Poem/api/tests/test_views_internal.py
+++ poem/Poem/api/tests/test_views_internal.py
@@ -12,9 +12,15 @@ class ListMetricsInGroupAPIViewTests(APITestCase):
         self.view = ListMetricsInGroup.as_view()
         self.url = '/api/v2/internal/metrics/EOSC'
         self.user = CustUser.objects.create(username='testuser')
-        metric1 = Metrics.objects.create(name='argo.AMS-Check')
+
+        metric1 = Metrics.objects.create(name='org.apel.APEL-Pub')
+        metric2 = Metrics.objects.create(name='org.apel.APEL-Sync')
+
         group1 = GroupOfMetrics.objects.create(name='EOSC')
         group1.metrics.create(name=metric1.name)
+        group1.metrics.create(name=metric2.name)
+
+        GroupOfMetrics.objects.create(name='Empty_group')
 
     def test_permission_denied_in_case_no_authorization(self):
         request = self.factory.get(self.url)
@@ -27,3 +33,14 @@ class ListMetricsInGroupAPIViewTests(APITestCase):
         force_authenticate(request, user=self.user)
         response = self.view(request, 'fake_group')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_metrics_in_group_for_a_given_group(self):
+        request = self.factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request, 'EOSC')
+        self.assertEqual(
+            response.data,
+            {
+                'result': ['org.apel.APEL-Pub', 'org.apel.APEL-Sync']
+            }
+        )
