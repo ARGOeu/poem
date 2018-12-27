26bb2e318433149d0f3275a4a4495f1f10e8d3f9
Katarina Zailac <katarina.zailac@gmail.com>
2018-12-27 09:26:19 +0100

    Added unit test for internal_views to check if 404 error is returned in case nonexisting site is requested.

--- poem/Poem/api/tests/test_views_internal.py
+++ poem/Poem/api/tests/test_views_internal.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.test import APITestCase
 
 from Poem.poem.models import *
 from Poem.api.views_internal import *
@@ -14,3 +14,8 @@ class ListMetricsInGroupAPIViewTests(APITestCase):
         request = self.factory.get(self.url)
         response = self.view(request)
         self.assertEqual(response.status_code, 403)
+
+    def test_status_code_in_case_nonexisting_site(self):
+        url = '/api/v2/internal/metric/fake_group'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
