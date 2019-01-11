9ced1d2ffe4f0a93bd1cffe3386351596499ce89
Katarina Zailac <katarina.zailac@gmail.com>
2019-01-11 11:54:43 +0100

    Created internal api where list of users can be obtained.

--- poem/Poem/api/urls_internal.py
+++ poem/Poem/api/urls_internal.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('metrics/<str:group>', views_internal.ListMetricsInGroup.as_view()),
     path('tokens/', views_internal.ListTokens.as_view()),
     path('tokens/<str:name>', views_internal.ListTokenForTenant.as_view()),
+    path('users/', views_internal.ListUsers.as_view()),
 ]
-
