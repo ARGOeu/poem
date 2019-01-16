62c03d3fc746def1ef1304009cba338a6a59e39d
Katarina Zailac <katarina.zailac@gmail.com>
2019-01-16 11:39:30 +0100

    New change comments in change view for LogEntry.

--- poem/Poem/poem/admin_interface/siteactions.py
+++ poem/Poem/poem/admin_interface/siteactions.py
@@ -1,6 +1,10 @@
 from gettext import gettext
+import json
 
 from django.contrib import admin
+from django.utils.text import get_text_list
+
+from Poem.poem.models import *
 
 
 def get_new_logentry_name(obj):
@@ -10,6 +14,89 @@ def get_new_logentry_name(obj):
         return gettext('Changed "%(object)s".' % {'object': obj.object_repr})
     elif obj.is_deletion():
         return gettext('Deleted "%(object)s".' % {'object': obj.object_repr})
+
+
+def get_object_from_db(obj):
+    obj = obj.split(' ')
+    model = obj[0]
+    pk = obj[2][1:-1]  # because the number is in parenthesis
+    if pk == 'None':
+        return ''
+    else:
+        if model in ['MetricDependancy', 'MetricFlags', 'MetricFiles',
+                     'MetricParameter', 'MetricFileParameter',
+                     'MetricAttribute', 'MetricConfig']:
+            code = 'obj_repr = %s.objects.get(id=%s).key' % (model, pk)
+        elif model in ['MetricParent', 'MetricProbeExecutable']:
+            code = 'obj_repr = %s.objects.get(id=%s).value' % (model, pk)
+        else:
+            code = 'obj_repr = %s.objects.get(id=%s).name' % (model, pk)
+        exec(code, globals())
+        return obj_repr
+
+
+def get_new_change_message(obj):
+    if obj.content_type.model in ['profile', 'API key', 'groupofmetrics']:
+        return obj.__str__()
+    else:
+        if obj.change_message and obj.change_message[0] == '[':
+            try:
+                change_message = json.loads(obj.change_message)
+            except json.JSONDecodeError:
+                return obj.change_message
+            messages = []
+            for sub_message in change_message:
+                if 'added' in sub_message:
+                    if sub_message['added']:
+                        sub_message['added']['name'] = \
+                            gettext(sub_message['added']['name'])
+                        if get_object_from_db(gettext(sub_message['added'][
+                                                          'object'])):
+                            messages.append(gettext('Added %s "%s".')
+                                            % (sub_message['added']['name'],
+                                               get_object_from_db(
+                                                   gettext(sub_message[
+                                                               'added'][
+                                                               'object'])
+                                               )))
+                        else:
+                            messages.append(gettext('Added a %s.'
+                                                    % sub_message['added'][
+                                                        'name']))
+                    else:
+                        messages.append(gettext('Added %s.' % obj.object_repr))
+                elif 'changed' in change_message:
+                    sub_message['changed']['fields'] = get_text_list(
+                        sub_message['changed']['fields'], gettext('and')
+                    )
+                    if 'name' in sub_message['changed']:
+                        sub_message['changed']['name'] = gettext(
+                            sub_message['changed']['name']
+                        )
+                        messages.append(
+                            gettext('Changed %s for %s "%s".'
+                                    % (sub_message['changed']['fields'],
+                                       sub_message['changed']['name'],
+                                       get_object_from_db(
+                                           gettext(sub_message['changed'][
+                                               'object'])
+                                       ))
+                                    )
+                        )
+                    else:
+                        messages.append(gettext('Changed {fields}.').format(
+                            **sub_message['changed']))
+                elif 'deleted' in sub_message:
+                    sub_message['deleted']['name'] = gettext(
+                        sub_message['deleted']['name']
+                    )
+                    messages.append(gettext('Deleted a %s.')
+                                    % sub_message['deleted']['name'])
+
+            change_message = ' '.join(msg[0].upper() + msg[1:] for msg in messages)
+            return change_message
+        else:
+            return obj.change_message
 
 
 class LogEntryAdmin(admin.ModelAdmin):
@@ -24,7 +111,7 @@ class LogEntryAdmin(admin.ModelAdmin):
     log_entry_name.short_description = 'Log entry'
 
     def new_change_message(self, obj):
-        return obj.__str__()
+        return get_new_change_message(obj)
     new_change_message.short_description = 'change message'
 
     list_display = (log_entry_name, 'user', 'action_time')
