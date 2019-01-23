de25e473842d6394536fc1519285cecadb79d54a
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-01-23 15:09:46 +0100

    refined build of test env that requires cloned source dir passed as argument

--- testenv/setup_test.sh
+++ testenv/setup_test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# directory with cloned Github branch code
+cd ${1}
+
 # load virtual environment
 source /etc/profile.d/venv_poem.sh
 workon poem
+
+pip3 install -r requirements.txt
+pip3 install -r requirements_tests.txt
+python setup.py egg_info --tag-date sdist bdist_wheel
+pip3 install dist/*
 
 # prerequisites
 SITEPACK=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')
