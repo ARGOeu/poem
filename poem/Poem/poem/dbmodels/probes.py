68cf6aa2f8f46d606b1a9f814987714fdae8bb25
Katarina Zailac <katarina.zailac@gmail.com>
2019-02-21 08:46:57 +0100

    created new field in Probe model

--- poem/Poem/poem/dbmodels/probes.py
+++ poem/Poem/poem/dbmodels/probes.py
@@ -19,6 +19,7 @@ class Probe(models.Model):
     group = models.CharField(max_length=1024)
     user = models.CharField(max_length=32, blank=True)
     datetime = models.DateTimeField(blank=True, max_length=32, null=True)
+    update_metric = models.NullBooleanField(default=True)
 
     class Meta:
         permissions = (('probesown', 'Read/Write/Modify'),)
