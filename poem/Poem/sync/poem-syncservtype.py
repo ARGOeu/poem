6be0227a0cdda16bca6f3d1c219fa8fbed02d1f0
Katarina Zailac <katarina.zailac@gmail.com>
2019-02-22 09:14:43 +0100

    refactored poem-syncservtypes.py to use Django ORM

--- poem/Poem/sync/poem-syncservtype.py
+++ poem/Poem/sync/poem-syncservtype.py
@@ -47,89 +47,105 @@ def main():
     for schema in schemas:
         with schema_context(schema):
             tenant = Tenant.objects.get(schema_name=schema)
-        data = tenant_servtype_data(tenant.name)
+            data = tenant_servtype_data(tenant.name)
 
-        Feed_List = None
-        fos = []
+            Feed_List = None
+            fos = []
 
-        try:
-            for fp in [settings.HOST_CERT, settings.HOST_KEY]:
-                if not os.path.exists(fp):
-                    raise IOError("invalid path %s" % (fp))
-                else:
-                    fos.append(open(fp))
-        except IOError as e:
-            logger.error(e)
-            raise SystemExit(1)
-        for fo in fos:
-            fo.close()
-
-        o = urlparse(data['SERVICETYPE_URL'])
-        try:
-            if o.scheme.startswith('https'):
-                context = ssl.create_default_context(cafile=settings.CAFILE,
-                                                     capath=settings.CAPATH)
-                conn = http.client.HTTPSConnection(host=o.netloc, \
-                                               key_file=settings.HOST_KEY, cert_file=settings.HOST_CERT, \
-                                               timeout=60,
-                                               context=context)
-            else:
-                conn = http.client.HTTPConnection(host=o.netloc)
-
-            headers = dict()
-            if data['HTTPAUTH']:
-                userpass_ascii = '{0}:{1}'.format(data['HTTPUSER'],
-                                                  data['HTTPPASS'])
-                userpass = base64.b64encode(userpass_ascii.encode())
-                headers={'Authorization': 'Basic ' + userpass.decode()}
-
-            conn.request('GET', o.path + '?' + o.query, headers=headers)
-            ret = conn.getresponse().read()
-
-        except Exception as e:
-            logger.error("Error service flavours feed - %s" % (repr(e)))
-            raise SystemExit(1)
-
-        try:
-            Root = ElementTree.XML(ret)
-        except Exception as e:
-            logger.error("Error parsing service flavours - %s" % (e))
-            raise SystemExit(1)
-
-        elements = Root.findall("SERVICE_TYPE")
-        if not elements:
-            logger.error("Error parsing service flavours")
-            raise SystemExit(1)
-
-        Feed_List = []
-        for element in elements:
-            Element_List = {}
-            if element.getchildren():
-                for child_element in element.getchildren():
-                    Element_List[str(child_element.tag).lower()] = (child_element.text)
-            Feed_List.append(Element_List)
-
-
-        sfindb = set([(sf.name, sf.description) for sf in models.ServiceFlavour.objects.all()])
-        if len(sfindb) != len(Feed_List):
-            sfs = set([(feed['service_type_name'], feed['service_type_desc']) \
-                    for feed in Feed_List])
-            cur = connection.cursor()
             try:
-                if len(sfindb) < len(Feed_List):
-                    cur.executemany('INSERT INTO poem_serviceflavour VALUES (?,?)', \
-                            sfs.difference(sfindb))
-                    logger.info("Added %d service flavours" %\
-                                (len(Feed_List) - len(sfindb)))
-                elif len(sfindb) > len(Feed_List):
-                    cur.executemany('DELETE FROM poem_serviceflavour WHERE name IN (?,?)', \
-                            sfindb.difference(sfs))
-                    logger.info("Deleted %d service flavours" %\
-                                (len(sfindb) - len(Feed_List)))
-                connection.close()
+                for fp in [settings.HOST_CERT, settings.HOST_KEY]:
+                    if not os.path.exists(fp):
+                        raise IOError("invalid path %s" % (fp))
+                    else:
+                        fos.append(open(fp))
+            except IOError as e:
+                logger.error(e)
+                raise SystemExit(1)
+            for fo in fos:
+                fo.close()
+
+            o = urlparse(data['SERVICETYPE_URL'])
+            try:
+                if o.scheme.startswith('https'):
+                    context = ssl.create_default_context(
+                        cafile=settings.CAFILE,
+                        capath=settings.CAPATH
+                    )
+                    conn = http.client.HTTPSConnection(
+                        host=o.netloc,
+                        key_file=settings.HOST_KEY,
+                        cert_file=settings.HOST_CERT,
+                        timeout=60,
+                        context=context
+                    )
+                else:
+                    conn = http.client.HTTPConnection(host=o.netloc)
+
+                headers = dict()
+                if data['HTTPAUTH']:
+                    userpass_ascii = '{0}:{1}'.format(data['HTTPUSER'],
+                                                      data['HTTPPASS'])
+                    userpass = base64.b64encode(userpass_ascii.encode())
+                    headers={'Authorization': 'Basic ' + userpass.decode()}
+
+                conn.request('GET', o.path + '?' + o.query, headers=headers)
+                ret = conn.getresponse().read()
+
             except Exception as e:
-                logger.error("database operations failed - %s" % e)
-        else:
-            logger.info("Service Flavours database is up to date")
+                logger.error("%s: Error service flavours feed - %s" % (
+                    schema.upper(), repr(e)))
+                raise SystemExit(1)
+
+            try:
+                Root = ElementTree.XML(ret)
+            except Exception as e:
+                logger.error("%s: Error parsing service flavours - %s" % (
+                    schema.upper(), e))
+                raise SystemExit(1)
+
+            elements = Root.findall("SERVICE_TYPE")
+            if not elements:
+                logger.error("%s: Error parsing service flavours"
+                             % schema.upper())
+                raise SystemExit(1)
+
+            Feed_List = []
+            for element in elements:
+                Element_List = {}
+                if element.getchildren():
+                    for child_element in element.getchildren():
+                        Element_List[str(child_element.tag).lower()] = (child_element.text)
+                Feed_List.append(Element_List)
+
+
+            sfindb = set([(sf.name, sf.description) for sf in models.ServiceFlavour.objects.all()])
+            if len(sfindb) != len(Feed_List):
+                sfs = set([(feed['service_type_name'], feed['service_type_desc']) \
+                        for feed in Feed_List])
+                try:
+                    if len(sfindb) < len(Feed_List):
+                        for flavour in sfs.difference(sfindb):
+                            models.ServiceFlavour.objects.create(
+                                name=flavour[0],
+                                description=flavour[1]
+                            )
+                        logger.info("%s: Added %d service flavours"
+                                    % (schema.upper(),
+                                       (len(Feed_List) - len(sfindb))))
+                    elif len(sfindb) > len(Feed_List):
+                        for flavour in sfindb.difference(sfs):
+                            models.ServiceFlavour.objects.filter(
+                                name=flavour[0],
+                                description=flavour[1]
+                            ).delete()
+                        logger.info("%s: Deleted %d service flavours"
+                                    % (schema.upper(),
+                                       (len(sfindb) - len(Feed_List))))
+                except Exception as e:
+                    logger.error("%s: database operations failed - %s"
+                                 % (schema.upper(), e))
+            else:
+                logger.info("%s: Service Flavours database is up to date"
+                            % schema.upper())
 
 main()
