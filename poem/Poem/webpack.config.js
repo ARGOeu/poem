9c024b190527de820e2c846f4ac9b66e2b7a8bfa
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-02-24 21:56:29 +0100

    working webpack bundle creation

--- poem/Poem/webpack.config.js
+++ poem/Poem/webpack.config.js
@@ -1,48 +1,28 @@
-const webpack = require("webpack")
-const path = require("path")
-const BundleTracker = require("webpack-bundle-tracker")
-const TerserPlugin = require("terser-webpack-plugin")
-//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    entry: "./assets/js/index.js",
-    node: {
-        fs: 'empty',
-        fsevents: 'empty',
-        child_process: 'empty',
-        module: 'empty'
-    },
-    output: {
-        path: path.resolve("./assets/bundles/"),
-        filename: "[name]-[hash].js",
-        chunkFilename: "[name]-[hash].js"
-    },
-    mode: 'production',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/env', '@babel/react']
-                }
-            }
-        ]
-    },
-    plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
-        //new BundleAnalyzerPlugin()
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-        new TerserPlugin({
-            cache: true,
-            parallel: true,
-            sourceMap: true,
-        })
-        ]
-    }
-}
+  context: __dirname,
+  entry: "./assets/js/index.js",
+  output: {
+      path: path.resolve("./assets/bundles/"),
+      filename: "[name]-[hash].js",
+      chunkFilename: "[name]-[hash].js"
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
