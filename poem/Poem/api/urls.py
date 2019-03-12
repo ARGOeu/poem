f9e64fb789c1ad3134a0430e791e1ef5c67bd134
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-03-12 20:33:29 +0100

    namespaced and named Django URLs
    Conflicts:
    	poem/Poem/urls.py

--- poem/Poem/api/urls.py
+++ poem/Poem/api/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import include, url
 
 from . import views
 
+app_name = 'poem'
+
 urlpatterns = [
     path('profiles/', views.ListProfile.as_view()),
     path('profiles/<str:name>', views.DetailProfile.as_view()),
     path('metrics/', views.ListMetrics.as_view()),
     path('metrics/<str:tag>', views.ListTaggedMetrics.as_view()),
-    path('internal/', include('Poem.api.urls_internal'))
+    path('internal/', include('Poem.api.urls_internal', namespace='internal'))
 ]
--- poem/Poem/api/urls_internal.py
+++ poem/Poem/api/urls_internal.py
@@ -2,14 +2,16 @@ from django.urls import path
 
 from . import views_internal
 
+app_name = 'poem'
+
 urlpatterns = [
-    path('metrics/<str:group>', views_internal.ListMetricsInGroup.as_view()),
-    path('tokens/', views_internal.ListTokens.as_view()),
-    path('tokens/<str:name>', views_internal.ListTokenForTenant.as_view()),
-    path('users/', views_internal.ListUsers.as_view()),
-    path('groups/', views_internal.ListGroupsForUser.as_view()),
-    path('groups/<str:group>', views_internal.ListGroupsForUser.as_view()),
-    path('probes/<str:probe_name>', views_internal.ListProbes.as_view()),
-    path('aggregations/', views_internal.ListAggregations.as_view()),
-    path('aggregations/<str:aggregation_name>', views_internal.ListAggregations.as_view()),
+    path('metrics/<str:group>', views_internal.ListMetricsInGroup.as_view(), name='metrics'),
+    path('tokens/', views_internal.ListTokens.as_view(), name='tokens'),
+    path('tokens/<str:name>', views_internal.ListTokenForTenant.as_view(), name='tokens'),
+    path('users/', views_internal.ListUsers.as_view(), name='users'),
+    path('groups/', views_internal.ListGroupsForUser.as_view(), name='groups'),
+    path('groups/<str:group>', views_internal.ListGroupsForUser.as_view(), name='groups'),
+    path('probes/<str:probe_name>', views_internal.ListProbes.as_view(), name='probes'),
+    path('aggregations/', views_internal.ListAggregations.as_view(), name='aggregations'),
+    path('aggregations/<str:aggregation_name>', views_internal.ListAggregations.as_view(), name='aggregations'),
 ]
