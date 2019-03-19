39fa791e33ff332aae3d57be2fe46b773f738df5
Katarina Zailac <katarina.zailac@gmail.com>
2019-03-19 10:42:01 +0100

    created UserProfile model in poem app

---
+++ poem/Poem/poem/dbmodels/user.py
@@ -0,0 +1,78 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
+
+from Poem.poem.dbmodels.aggregations import GroupOfAggregations
+from Poem.poem.dbmodels.metricstags import GroupOfMetrics
+from Poem.poem.dbmodels.probes import GroupOfProbes
+from Poem.poem.dbmodels.profiles import GroupOfProfiles
+
+
+class UserProfile(models.Model):
+    """
+    Extension of auth.User model that adds certificate DN.
+    """
+    class Meta:
+        app_label = 'poem'
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    subject = models.CharField(
+        'distinguishedName',
+        max_length=512,
+        blank=True,
+        null=True
+    )
+    egiid = models.CharField(
+        'eduPersonUniqueId',
+        max_length=255,
+        blank=True,
+        null=True,
+        unique=True
+    )
+    displayname = models.CharField(
+        'displayName',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    groupsofprofiles = models.ManyToManyField(
+        GroupOfProfiles,
+        verbose_name=_('groups of profiles'),
+        blank=True,
+        help_text=_('The groups of profiles that user will control.'),
+        related_name='user_set',
+        related_query_name='user'
+    )
+    groupsofmetrics = models.ManyToManyField(
+        GroupOfMetrics,
+        verbose_name=_('groups of metrics'),
+        blank=True,
+        help_text=_('The groups of metrics that user will control'),
+        related_name='user_set',
+        related_query_name='user'
+    )
+    groupsofprobes = models.ManyToManyField(
+        GroupOfProbes,
+        verbose_name=('groups of probes'),
+        blank=True,
+        help_text=_('The groups of probes that user will control'),
+        related_name='user_set',
+        related_query_name='user')
+    groupsofaggregations = models.ManyToManyField(
+        GroupOfAggregations,
+        verbose_name=_('groups of aggregations'),
+        blank=True,
+        help_text=_('The groups of aggregations that user will control'),
+        related_name='user_set',
+        related_query_name='user'
+    )
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+post_save.connect(create_user_profile, sender=settings.AUTH_USER_MODEL)
