bc9386ef07562785449f8bda174874d8b8dcf9c9
Katarina Zailac <katarina.zailac@gmail.com>
2019-03-19 10:32:44 +0100

    removed CustUser model from poem app

--- poem/Poem/poem/models.py
+++ poem/Poem/poem/models.py
@@ -1,6 +1,5 @@
 from Poem.poem.dbmodels.probes import *
 from Poem.poem.dbmodels.profiles import *
-from Poem.poem.dbmodels.user import *
 from Poem.poem.dbmodels.metricstags import *
 from Poem.poem.dbmodels.rever import *
 from Poem.poem.dbmodels.services import *
