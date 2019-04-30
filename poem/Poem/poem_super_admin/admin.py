fbcbb5c78d53d3e0e52b74c9f8aca3f634d26b98
Katarina Zailac <katarina.zailac@gmail.com>
2019-04-30 13:44:24 +0200

    registered Probe model with poem_super_admin admin

--- poem/Poem/poem_super_admin/admin.py
+++ poem/Poem/poem_super_admin/admin.py
@@ -3,9 +3,11 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.cache import never_cache
 
+from Poem.poem_super_admin.admin_interface.siteprobes import ProbeAdmin
 from Poem.poem_super_admin.admin_interface.sitetenant import TenantAdmin
 from Poem.poem_super_admin.admin_interface.userprofile import \
     SuperUserProfileAdmin
+from Poem.poem_super_admin.models import Probe
 from Poem.tenants.models import Tenant
 from Poem.users.models import CustUser
 
@@ -22,7 +24,7 @@ class SuperAdminSite(AdminSite):
             if request.user.is_superuser:
                 app_list = self.get_app_list(request)
 
-                order = ['tenants', 'users']
+                order = ['poem_super_admin', 'tenants', 'users']
                 app_list = sorted(
                     app_list, key=lambda a: order.index(a['app_label'])
                 )
@@ -46,3 +48,4 @@ class SuperAdminSite(AdminSite):
 mysuperadmin = SuperAdminSite(name='superadmin')
 mysuperadmin.register(Tenant, TenantAdmin)
 mysuperadmin.register(CustUser, SuperUserProfileAdmin)
+mysuperadmin.register(Probe, ProbeAdmin)
