6ac097ee3a315b6efde7208b1623f76c72f905ce
Katarina Zailac <katarina.zailac@gmail.com>
2019-06-07 13:47:54 +0200

    added new lookups for ajax fields

--- poem/Poem/urls_public.py
+++ poem/Poem/urls_public.py
@@ -1,3 +1,4 @@
+from ajax_select import urls as ajax_select_urls
 from django.conf.urls import include
 from django.http import HttpResponseRedirect
 from django.urls import re_path
@@ -7,6 +8,7 @@ from Poem.poem_super_admin.admin import mysuperadmin
 urlpatterns = [
     re_path(r'^$', lambda x: HttpResponseRedirect('/poem/superadmin/')),
     re_path(r'^superadmin/', mysuperadmin.urls),
+    re_path(r'^admin/lookups/', include(ajax_select_urls)),
     re_path(r'^saml2/', include(('djangosaml2.urls', 'poem'),
                                 namespace='saml2')),
 ]
